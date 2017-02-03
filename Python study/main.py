9d72b1d2aeedde7017795b457ba06af5b92241e5
atmos_tic <hantai8118@gmail.com>
2017-02-03 23:13:27 +0900

    20170203ver2

--- Python study/main.py
+++ Python study/main.py
@@ -1,8 +1,15 @@
 # coding: Shift_JIS
+import random
+f = open('greeting.txt')
+lines = f.readlines() 
+f.close()
+def responce():
+	print(random.choice(lines), end='')
 AIname = 'at'
 print("%s>"%"???", end='')
 print("こんにちは、私の名前は%sです。"%AIname)
 print("%s >"%AIname, end='')
+
 print("あなたの名前を教えてください。")
 print("user>", end='')
 username = input()
@@ -12,8 +19,11 @@ print("%s >"%username, end='')
 talk = input()
 while talk != '':
 	print("%s >"%AIname, end='')
-	print("%sって何ですか？"%talk)
+	#print("%sって何ですか？"%talk)
+	responce()
 	print("%s >"%username, end='')
 	talk = input()
 print("%s >"%AIname, end='')
 print("さようなら、%sさん"%username)
+
+
